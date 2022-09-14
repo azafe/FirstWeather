ec4ab421f8cc59d18a9d7e8feef4fa87f606a247
Fernando Zafe <fernandozafe@gmail.com>
2022-09-14 18:55:23 -0300

    Initial Commit

---
+++ FirstWeather/FirstWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstWeatherApp.swift
+//  FirstWeather
+//
+//  Created by Fernando Zafe on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
